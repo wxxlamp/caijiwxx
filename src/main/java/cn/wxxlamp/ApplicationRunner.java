ebb9bc720de15c69842b04a61de9df2693d49128
stalern <stalern2000@163.com>
2020-12-31 16:14:55 +0800

    消除代码中的魔数

---
+++ src/main/java/cn/wxxlamp/ApplicationRunner.java
@@ -0,0 +1,29 @@
+package cn.wxxlamp;
+
+import cn.wxxlamp.model.TreasureBox;
+import cn.wxxlamp.service.TreasureBoxService;
+
+/**
+ * 主方法运行类
+ * @author wxxlamp
+ * @date 2020/12/31~15:36
+ */
+public class ApplicationRunner {
+
+    /**
+     * 箱子的数目
+     */
+    private static final Integer BOX_NUM = 10;
+
+    /**
+     * hash值前五位的状态
+     */
+    private static final String BEGIN_STR = "00000";
+
+    public static void main(String[] args) {
+        TreasureBoxService treasureBoxService = new TreasureBoxService();
+        TreasureBox[] treasureBoxes = treasureBoxService.createTreasureBoxes(BOX_NUM, BEGIN_STR);
+        Integer[] magicNums = treasureBoxService.getMagicNumsFromTreasureBox(treasureBoxes);
+        treasureBoxService.printMagicNums(magicNums);
+    }
+}
