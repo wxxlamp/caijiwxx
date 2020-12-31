104bad410b9bb3d8b3aefb7f6739f9f2ee0217a8
stalern <stalern2000@163.com>
2020-12-31 15:33:03 +0800

    1. 创建宝箱实体类

---
+++ src/main/java/cn/wxxlamp/model/TreasureBox.java
@@ -0,0 +1,80 @@
+package cn.wxxlamp.model;
+
+import cn.wxxlamp.util.EncryptUtils;
+
+/**
+ * 宝箱实体类
+ * @author wxxlamp
+ * @date 2020/12/31~15:00
+ */
+public class TreasureBox {
+
+    /**
+     * 宝箱编号
+     */
+    private Integer number;
+
+    /**
+     * 开启宝箱的时间戳
+     */
+    private Long timeStamp;
+
+    /**
+     * 上个宝箱的hash值，
+     * 第一个宝箱的hash值为0，
+     * 采用sha256方式加密
+     */
+    private String preHash;
+
+    /**
+     * 当前宝箱的hash值
+     */
+    private String curHash;
+    /**
+     * 神秘数字
+     */
+    private Integer magicNumber;
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public Long getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(Long timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public String getPreHash() {
+        return preHash;
+    }
+
+    public void setPreHash(String preHash) {
+        this.preHash = preHash;
+    }
+
+    public String getCurHash() {
+        String allAttribute = this.number.toString() + this.timeStamp.toString()
+                + this.preHash + this.magicNumber.toString();
+        this.curHash = EncryptUtils.string2Sha256(allAttribute);
+        return curHash;
+    }
+
+    public void setCurHash(String curHash) {
+        this.curHash = curHash;
+    }
+
+    public Integer getMagicNumber() {
+        return magicNumber;
+    }
+
+    public void setMagicNumber(Integer magicNumber) {
+        this.magicNumber = magicNumber;
+    }
+}
